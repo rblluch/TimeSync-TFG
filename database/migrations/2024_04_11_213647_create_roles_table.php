a46ba891919bd445791d82e3bb4a8e1c604705d8
rblluch <rblluch03@gmail.com>
2024-04-12 00:47:02 +0200

    Update  permissions and add tables to bbdd

---
+++ database/migrations/2024_04_11_213647_create_roles_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->timestamps();
+        });
+
+        DB::table('roles')->insert([
+            [
+                'name' => 'superadmin', 
+                'description' => 'Super Administrator'
+            ],
+            [
+                'name' => 'admin', 
+                'description' => 'Administrator'
+            ],
+            [
+                'name' => 'user', 
+                'description' => 'Regular User'
+            ],
+        ]);
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('roles');
+    }
+};
